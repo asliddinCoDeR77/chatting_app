e18b96637922ff1e8d0a8268733bfc45da4a5054
ASLIDDINCODER <147050428+ASLIDDINCODERn1@users.noreply.github.com>
2024-06-11 13:04:35 +0500

    uptdate

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:dars54_authentication/view_models/cars_viewmodel.dart';
 import 'package:dars54_authentication/views/screens/login_screen.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +14,48 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final carsViewModel = CarsViewmodel();
   final nameController = TextEditingController();
+  DateTime? _startTime;
+  Timer? _logoutTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startTime = DateTime.now();
+    _startLogoutTimer();
+  }
+
+  @override
+  void dispose() {
+    _logoutTimer?.cancel();
+    nameController.dispose();
+    super.dispose();
+  }
+
+  void _startLogoutTimer() {
+    _logoutTimer = Timer.periodic(const Duration(seconds: 10), (timer) {
+      final currentTime = DateTime.now();
+      final elapsedTime = currentTime.difference(_startTime!).inSeconds;
+      if (elapsedTime >= 3600) {
+        _logoutUser();
+      }
+    });
+  }
+
+  void _logoutUser() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.remove("userData");
+
+    if (mounted) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (ctx) {
+            return const LoginScreen();
+          },
+        ),
+      );
+    }
+  }
 
   void addCar() async {
     await carsViewModel.addCar(nameController.text);
@@ -27,20 +70,7 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Bosh Sahifa"),
         actions: [
           IconButton(
-            onPressed: () async {
-              SharedPreferences sharedPreferences =
-                  await SharedPreferences.getInstance();
-              sharedPreferences.remove("userData");
-
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (ctx) {
-                    return const LoginScreen();
-                  },
-                ),
-              );
-            },
+            onPressed: _logoutUser,
             icon: const Icon(Icons.logout),
           ),
         ],
@@ -71,32 +101,33 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Expanded(
               child: FutureBuilder(
-                  future: carsViewModel.cars,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
+                future: carsViewModel.cars,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
 
-                    final cars = snapshot.data;
+                  final cars = snapshot.data;
 
-                    return cars == null || cars.isEmpty
-                        ? const Center(
-                            child: Text("Mashinalar mavjud emas!"),
-                          )
-                        : ListView.builder(
-                            itemCount: cars.length,
-                            itemBuilder: (ctx, index) {
-                              return Card(
-                                color: Colors.amber,
-                                child: ListTile(
-                                  title: Text(cars[index].name),
-                                ),
-                              );
-                            },
-                          );
-                  }),
+                  return cars == null || cars.isEmpty
+                      ? const Center(
+                          child: Text("Mashinalar mavjud emas!"),
+                        )
+                      : ListView.builder(
+                          itemCount: cars.length,
+                          itemBuilder: (ctx, index) {
+                            return Card(
+                              color: Colors.amber,
+                              child: ListTile(
+                                title: Text(cars[index].name),
+                              ),
+                            );
+                          },
+                        );
+                },
+              ),
             )
           ],
         ),
